992f8b61e538cde5c91e981a33b764c420f438ef
Cleankz <92687535+Cleankz@users.noreply.github.com>
2021-12-06 20:53:54 +0300

    Create unmanned.py

---
+++ unmanned.py
@@ -0,0 +1,32 @@
+def Unmanned(L, N,track):
+    time = L
+    l_time = 0
+    for i in range(L):
+        for j in range(N):
+            if i == track[j][0] and j == 0:
+                if i > track[j][1] and i < track[j][1] + track[j][2]:
+                    break
+                elif i <= track[j][1]:
+                    time = time + abs(track[j][1] - i)
+                    l_time = l_time + abs(track[j][1] - i)
+                    break
+                elif abs((track[j][1] + track[j][2]) - i) < track[j][1]:
+                    time = time + abs(track[j][1] - i)
+                    l_time = l_time + abs(track[j][1] - i)
+                    break
+                elif abs((track[j][1] + track[j][2]) - i) > track[j][1] and abs((track[j][1] + track[j][2]) - i) < track[j][2] :
+                    break
+            elif i == track[j][0] and j > 0:
+                if (i + l_time) > track[j][1] and (i + l_time) < track[j][1] + track[j][2]:
+                    break
+                elif (i + l_time) < track[j][1]:
+                    time = time + abs(track[j][1] - i)
+                    l_time = l_time + abs(track[j][1] - i)
+                    break
+                elif abs((track[j][1] + track[j][2]) - (i + l_time)) < track[j][1]:
+                    time = time + abs(track[j][1] - i)
+                    l_time = l_time + abs(track[j][1] - i)
+                    break
+                elif abs((track[j][1] + track[j][2]) - (i + l_time)) > track[j][1] and abs((track[j][1] + track[j][2]) -(i + l_time)) < track[j][2] :
+                    break
+    return time
